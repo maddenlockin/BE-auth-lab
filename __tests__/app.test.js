e019a47278e24a21ed07244f6ce9f75c50c09564
maddenlockin <maddenlockin@gmail.com>
2021-10-07 15:45:45 -0700

    built out model, service, controller for POST signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,23 +4,23 @@ const request = require('supertest');
 const app = require('../lib/app.js');
 
 describe('auth lab routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
-
-
-  it('verifies POST route to sign up user', async () => {
-    const res = await request(app)
-      .post('/api/v1/auth/signup')
-      .send({ email: 'dani@duck.com', password: 'p4ssw0rd' });
-
-    expect(res.body).toEqual({
-      id: expect.any(String),
-      email: 'dani@duck.com',
+    beforeEach(() => {
+        return setup(pool);
     });
-  });
 
-  afterAll(() => {
-    pool.end();
-  });
+
+    it('verifies POST route to sign up user', async () => {
+        const res = await request(app)
+            .post('/api/v1/auth/signup')
+            .send({ email: 'dani@duck.com', password: 'p4ssw0rd' });
+
+        expect(res.body).toEqual({
+            id: expect.any(String),
+            email: 'dani@duck.com',
+        });
+    });
+
+    afterAll(() => {
+        pool.end();
+    });
 });
