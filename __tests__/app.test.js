bb41952c7a9e2080113419e20f90e240f3611308
maddenlockin <maddenlockin@gmail.com>
2021-10-07 16:07:37 -0700

    POST sign up test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,14 +10,15 @@ describe('auth lab routes', () => {
 
 
     it('verifies POST route to sign up user', async () => {
-        const res = await request(app)
+        return request(app)
             .post('/api/v1/auth/signup')
-            .send({ email: 'dani@duck.com', password: 'p4ssw0rd' });
-
-        expect(res.body).toEqual({
-            id: expect.any(String),
-            email: 'dani@duck.com',
-        });
+            .send({ email: 'dani@duck.com', password: 'p4ssw0rd' })
+            .then((res) => {
+                expect(res.body).toEqual({
+                    id: expect.any(String),
+                    email: 'dani@duck.com',
+                });
+            });
     });
 
     afterAll(() => {
